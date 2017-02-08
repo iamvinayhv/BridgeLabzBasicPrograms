c025305081ea9f4fcd13444856d38c73634952b6
iamvinayhv <vinayhv62>
2017-02-08 18:28:55 +0530

    updated

---
+++ PowerOfNumber.java
@@ -0,0 +1,30 @@
+//For perticular Number
+
+class PowerOfNumber
+{
+	public static void main(String[]args)
+	{
+		
+		int n=Integer.parseInt(args[0]);
+
+		for(int i=0;i<31;i++)
+		{
+			int res=pow(n,i);
+			System.out.println(n+"^"+i+" = "+res); 
+		}
+		
+		
+	}
+	
+	static int pow(int num,int c)
+	{
+		int p=1;
+		while(c>0)
+		{
+			p=p*num;
+			c--;
+		}
+		return p;
+	}
+		
+}
